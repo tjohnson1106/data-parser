de7d995e70d5dac9d3fb6e2697709dcd12ff2698
Thomas Johnson <tjohnson1106@gmail.com>
2024-07-19 19:41:55 -0400

    created data parse function

--- lib/data_parser.ex
+++ lib/data_parser.ex
@@ -1,10 +1,21 @@
 defmodule ElhexDelivery.PostalCode.DataParser do
-@postal_codes_filepath "data/postal_national.txt"
+  @postal_codes_filepath "data/postal_national.txt"
 
-def parse_data do
-  [header | data_rows] = File.read!(@postal_codes_filepath) |> String.split("\n")
+  def parse_data do
+    [_header | data_rows] = File.read!(@postal_codes_filepath) |> String.split("\n")
 
-  data_rows
-end
-
+    data_rows
+    |> Enum.map(fn row ->
+      String.split(row, "\t")
+    end)
+    |> Enum.filter(fn row ->
+      case row do
+        [postal_code, _, _, _, _, latitude, longitude] -> true
+        _ -> false
+      end
+    end)
+    |> Enum.map(fn row ->
+      []
+    end)
+  end
 end
--- test/postal_code/data_parser_test.ex
+++ test/postal_code/data_parser_test.ex
@@ -5,5 +5,7 @@ defmodule ElhexDelivery.PostalCode.DataParserTest do
 
   test "parse_data" do
     data_rows = DataParser.parse_data()
+    first_row = Enum.at(data_rows, 0)
+    IO.inspect(first_row)
   end
 end
